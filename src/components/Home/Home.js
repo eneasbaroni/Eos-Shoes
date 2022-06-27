40ffe9d1f78068b5b3d7ec65df5f9dfffbd181d5
Eneas <87401725+eneasbaroni@users.noreply.github.com>
2022-06-27 11:00:57 -0300

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import otw from "../../assets/otw.png";
 import "./home.css"
 
 const Home = () => {
+  const [loading, setLoading] = useState(true)
+
+  function handleLoad () {
+    setLoading(false); 
+  }
+
   return (
   <>
-    <div className="container-fluid" id="index">
-      <div className="row">
-        <div className="col-12 col-lg-7 homeImg"></div>                   
-        <div className="col-12 col-lg-5 home">
-          <img src={otw} className="otw" alt="off the wall"></img>
-        </div>  
-      </div>
-    </div>            
+    {loading
+      ?<img src={otw} style={{display: "none"}} alt="preload" onLoad={handleLoad}/>
+      : <div className="container-fluid" id="index">
+          <div className="row">
+            <div className="col-12 col-lg-7 homeImg"></div>                   
+            <div className="col-12 col-lg-5 home">
+              <img src={otw} className="otw" alt="off the wall"></img>
+            </div>  
+          </div>
+        </div> 
+    }
+             
   </>
 )
 }
