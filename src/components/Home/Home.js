d1e8928d0965a5bfe9ee6237be69e5f45b1e2c48
Eneas <87401725+eneasbaroni@users.noreply.github.com>
2022-06-27 11:04:42 -0300

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import otw from "../../assets/otw.png";
+import Loader from "../Loader/Loader";
 import "./home.css"
 
 const Home = () => {
@@ -10,21 +11,24 @@ const Home = () => {
   }
 
   return (
-  <>
-    {loading
-      ?<img src={otw} style={{display: "none"}} alt="preload" onLoad={handleLoad}/>
-      : <div className="container-fluid" id="index">
-          <div className="row">
-            <div className="col-12 col-lg-7 homeImg"></div>                   
-            <div className="col-12 col-lg-5 home">
-              <img src={otw} className="otw" alt="off the wall"></img>
-            </div>  
-          </div>
-        </div> 
-    }
-             
-  </>
-)
+    <>
+      {loading
+        ? <>
+            <Loader/>
+            <img src={otw} style={{display: "none"}} alt="preload" onLoad={handleLoad}/>
+          </>
+        : <div className="container-fluid" id="index">
+            <div className="row">
+              <div className="col-12 col-lg-7 homeImg"></div>                   
+              <div className="col-12 col-lg-5 home">
+                <img src={otw} className="otw" alt="off the wall"></img>
+              </div>  
+            </div>
+          </div> 
+      }
+              
+    </>
+  )
 }
 
 export default Home;
