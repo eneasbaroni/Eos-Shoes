c76337f23bf6d33787c46c2a3c838571bf504c25
Eneas <87401725+eneasbaroni@users.noreply.github.com>
2022-06-27 10:55:13 -0300

    Update CategoryContainer.js

--- src/views/CategoryContainer/CategoryContainer.js
+++ src/views/CategoryContainer/CategoryContainer.js
@@ -1,7 +1,9 @@
+import {  useState } from 'react'
 import { Link } from 'react-router-dom'
+import Loader from '../../components/Loader/Loader'
 import "./categoryContainer.css"
 
-const CategorySelector = ({category}) => {
+const CategorySelector = ({category}) => {  
   return (
     <div className="col-12 col-sm-6">
       <Link to={`/category/${category}`}> 
@@ -12,16 +14,33 @@ const CategorySelector = ({category}) => {
 }
 
 const Category = () => {
-  return (    
-    <div className='row'>
-      <div className="col-lg-1"></div>                    
-      <div className="col-12 col-lg-11 row categorySeletors"> 
-        <CategorySelector category={"all"} />
-        <CategorySelector category={"urban"} />
-        <CategorySelector category={"sport"} />
-        <CategorySelector category={"mountain"} />
-      </div>
-    </div>    
+  const [loading, setLoading] = useState(true)
+
+  function handleLoad () {
+    setLoading(false); 
+  }
+
+  return (   
+    <>
+      {loading
+        ? <>
+            <Loader/>
+            <img src={`/images/all.jpg`} style={{display: "none"}} alt="preload"/>
+            <img src={`/images/urban.jpg`} style={{display: "none"}} alt="preload"/>
+            <img src={`/images/sport.jpg`} style={{display: "none"}} alt="preload"/>
+            <img  src={`/images/mountain.jpg`} style={{display: "none"}} alt="preload" onLoad={handleLoad} />           
+          </>
+        :<div className='row'>
+            <div className="col-lg-1"></div>                    
+            <div className="col-12 col-lg-11 row categorySeletors"> 
+              <CategorySelector category={"all"} />
+              <CategorySelector category={"urban"} />
+              <CategorySelector category={"sport"} />
+              <CategorySelector category={"mountain"} />                           
+            </div>
+          </div> 
+      }         
+    </>
   )
 }
 
